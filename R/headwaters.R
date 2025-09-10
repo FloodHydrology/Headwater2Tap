189cbec4417f420fa85e30f65c681bbde9ac3c70
FloodHydrology <cnjones7@ua.edu>
2025-09-10 13:53:36 -0500

    apply fun

--- R/headwaters.R
+++ R/headwaters.R
@@ -104,129 +104,13 @@ fun <- function(n){
   output
 }
 
+#Apply function
+t0<-Sys.time()
+df <- 
+  lapply(
+    X = seq(1, 25), 
+    FUN = fun) %>% 
+  bind_rows()
+tf<-Sys.time()
+tf-t0
 
-
-# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# # 3.0 Plots --------------------------------------------------------------------
-# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# # 3.1 Density map --------------------------------------------------------------
-# #prep pwi_point data
-# pwi_point <- pwi_point %>% 
-#   mutate(
-#     lat = st_coordinates(pwi_point)[,1], 
-#     lon = st_coordinates(pwi_point)[,2],
-#     d_wwtps_100 = d_wwtps*100) %>% 
-#   filter(d_wwtps_100<1)
-# 
-# #Create density map
-# density_map <- pwi_point %>%
-#   arrange(d_wwtps) %>% 
-#   ggplot()+
-#   geom_sf(data = states, lwd=0.5) + 
-#   geom_point(
-#     aes(
-#       x=lat,
-#       y=lon,
-#       color = d_wwtps_100, 
-#       size =  d_wwtps_100, 
-#       alpha = d_wwtps_100, 
-#       #shape= NP
-#     )) +
-#   scale_color_distiller(
-#     palette = 'Spectral',
-#     direction = -1, 
-#     breaks = seq(0,1,25)) +
-#   scale_size_continuous(
-#     range = c(0.5, 3.5), 
-#     breaks = seq(0,1,25)) +
-#   scale_alpha_continuous(
-#     range = c(0.5,.8) , 
-#     breaks = seq(0,1,25)) +
-#   #scale_shape(labels = c("Perennial", "Non-perennial")) +
-#   guides(
-#     color = guide_colorbar(
-#       title ="WWTP Density", 
-#       order = 1),
-#     size="none", 
-#     alpha = "none"
-#   ) +
-#   theme_bw() +
-#   theme(
-#     legend.position = 'right',
-#     legend.justification = "center") +
-#   xlab(NULL) +
-#   ylab(NULL) 
-# 
-# density_map
-# 
-# # 3.2 Histogram ----------------------------------------------------------------
-# pwi_point$d_wwpts_cut <- cut(pwi_point$d_wwtps_100, c(-1, .001, 0.1, 0.25, 0.5, 1)) #create flow categorices
-# 
-# pwi_density <- pwi_point %>% 
-#   ggplot(aes(x=d_wwpts_cut)) +
-#   geom_bar() +
-#   scale_x_discrete(
-#     name = "Upstream WWTP Density [WWTP/km^2]", 
-#     labels = c("0", "0-0.1", "0.1-1.25", "0.25-0.5",">0.5")) +
-#   scale_y_continuous(name = "Number of Drinking\nWater Intakes") +
-#   theme_classic() +
-#   theme(
-#     axis.title = element_text(size = 14), 
-#     axis.text  = element_text(size = 10)
-#   )
-# 
-# # 3.2 Bama ---------------------------------------------------------------------
-# #Create bama shape
-# bama_shp <- states() %>% filter(STUSPS == "AL") %>% st_transform(., 5070)
-# 
-# #Subset shapes to bama
-# wwtp_bama <- wwtp[bama_shp,]
-# pwi_bama <- pwi_point[bama_shp,]
-# 
-# #PWI Sheds
-# pwi_sheds_bama <- pwi_shed %>% filter(DVSN_ID %in% pwi_bama$DVSN_ID)
-# pwi_sheds_bama <- st_crop(pwi_sheds_bama, bama_shp)
-# 
-# #Add coords to wwtp_bama
-# wwtp_bama <- wwtp_bama %>% 
-#   mutate(
-#     lat = st_coordinates(wwtp_bama)[,1], 
-#     lon = st_coordinates(wwtp_bama)[,2])
-# 
-# 
-# #Create bama map
-# #Create density map
-# bama_map<-bama_shp %>%
-#   ggplot()+
-#   geom_sf(data = bama_shp, lwd=0.5) + 
-#   geom_sf(data = pwi_sheds_bama, lwd=0.5, bg="light blue")+
-#   geom_point(
-#     data= wwtp_bama, 
-#     aes(x = lat, y = lon), 
-#     pch = 19, 
-#     cex = 1, 
-#     col = "brown", 
-#     alpha = 0.6) + 
-#   geom_point(
-#     data = pwi_bama,
-#     aes(x=lat,y=lon),
-#     pch = 19,
-#     cex = 4, 
-#     color = "dark blue") +
-#   theme_bw() +
-#   theme(
-#     legend.position = 'none',
-#     legend.justification = "center") +
-#   xlab(NULL) +
-#   ylab(NULL) 
-# 
-# 
-# # 3.3 Print! -------------------------------------------------------------------
-# 
-# #Write shape
-# st_write(bama_shp, "C:\\WorkspaceR\\Bama_Antibiotic_Resistance\\data\\surface_water_intakes\\bama.shp")
-# 
-# #Plot
-# ggsave(plot = pwi_density, file = 'docs/density.png', width = 4.25, height = 3, units = "in", dpi = 300)
-# ggsave(plot = density_map, file = "docs/density_map.png", width = 6, height = 3.75, units="in", dpi = 300)
-# ggsave(plot = bama_map, file = "docs/bama_map.png", width = 3, height = 4, units="in", dpi = 300)
